4abeb0a61d2cdb48baec879189be7335cd860e50
Zye <77932817+zyeworld@users.noreply.github.com>
2025-05-26 08:15:14 +0000

    refactor: Space travel to styleCalculator.ts

--- src/cursor/cursorMover.ts
+++ src/cursor/cursorMover.ts
@@ -1,6 +1,9 @@
 import * as vscode from 'vscode';
+import { StyleCalculator, CssObject } from '../util/styleCalculator';
 
-// State for Space Travel: space-based movement when space is pressed
+/**
+ * Bounding box used to denote a 'space' in SpaceState
+ */
 type Box = {
     minX: number,
     maxX: number,
@@ -8,65 +11,105 @@ type Box = {
     maxY: number
 }
 
-type SpaceTravelState = {
+/**
+ * State for Space Travel: space-based movement when space is pressed
+ */
+type SpaceState = {
     active: boolean,
     charWidth: number,
     charHeight: number,
-    currentSpace: Box,
+    space: Box,
     previousSpace: Box,
-    spaceStyle: vscode.TextEditorDecorationType | null
+    decoration: vscode.TextEditorDecorationType | null
+}
+
+/**
+ * Direction used in VSCode's 'cursorMove' command
+ */
+export enum Direction {
+    UP = 'up',
+    DOWN = 'down',
+    LEFT = 'left',
+    RIGHT = 'right'
 }
 
 export class CursorMover {
-    public static moveUp(units: number = 1) {
-        vscode.commands.executeCommand('cursorMove', {
-            "to": "up",
-            "value": units
-        });
-    }
-
-    public static moveDown(units: number = 1) {
-        vscode.commands.executeCommand('cursorMove', {
-            "to": "down",
-            "value": units
-        });
-    }
-
-    public static moveLeft(units: number = 1) {
-        vscode.commands.executeCommand('cursorMove', {
-            "to": "left",
-            "value": units
-        });
-    }
-
-    public static moveRight(units: number = 1) {
-        vscode.commands.executeCommand('cursorMove', {
-            "to": "right",
-            "value": units
-        });
-    }
-
-    private spaceTravelState: SpaceTravelState = {
+    private spaceState: SpaceState = {
         active: false,
         charWidth: 1,
-        charHeight: 14,
-        currentSpace: { minX: 0, maxX: 0, minY: 0, maxY: 0 },
+        charHeight: 19,
+        space: { minX: 0, maxX: 0, minY: 0, maxY: 0 },
         previousSpace: { minX: 0, maxX: 0, minY: 0, maxY: 0 },
-        spaceStyle: null
+        decoration: null
     };
 
-    private spaceTravel(editor: vscode.TextEditor, newSpace: Box) {
-        // Update space
+    /**
+     * Move cursor to a certain direction
+     * @param direction one of four directions
+     * @param units amount to move
+     */
+    public move(direction: Direction, units: number = 1) {
+        vscode.commands.executeCommand('cursorMove', {
+            to: direction,
+            value: units
+        });
+    }
+
+    // Start a new space travel
+    private startSpaceTravel(editor: vscode.TextEditor) {
+        const lineStart = editor.visibleRanges[0].start.line;
+        const lineEnd = editor.visibleRanges[0].end.line;
+
+        // Compute minimum and maximum possible x values
+        let minX = 1000, maxX = 0;
+        for (let i = lineStart; i <= lineEnd; i++) {
+            minX = Math.min(minX, editor.document.lineAt(i).firstNonWhitespaceCharacterIndex);
+            maxX = Math.max(maxX, editor.document.lineAt(i).range.end.character);
+        }
+
+        const newSpace: Box = { minX, maxX, minY: 0, maxY: lineEnd - lineStart };
+    }
+
+    // Stop the current space travel
+    private stopSpaceTravel(editor: vscode.TextEditor) {
+        this.spaceState.active = false;
+        if (this.spaceState.decoration) {
+            editor.setDecorations(this.spaceState.decoration, []);
+        }
+        this.spaceState.decoration = null;
+        return;
+    }
+
+    // Halve the box in a certain direction
+    private spaceTravel(editor: vscode.TextEditor, direction: Direction) {
         // 1. Update spaceTravelState
-        this.spaceTravelState.active = true;
-        this.spaceTravelState.previousSpace = this.spaceTravelState.currentSpace;
-        this.spaceTravelState.currentSpace = newSpace;
+        this.spaceState.active = true;
+        this.spaceState.previousSpace = this.spaceState.space;
+
+        // 1. Calculate new box
+        let midY = Math.floor((this.spaceState.space.minY + this.spaceState.space.maxY) / 2);
+        let midX = Math.floor((this.spaceState.space.minX + this.spaceState.space.maxX) / 2);
+        switch (direction) {
+            case Direction.UP:
+                this.spaceState.space = { ...this.spaceState.previousSpace, maxY: midY }
+                break;
+            case Direction.DOWN:
+                this.spaceState.space = { ...this.spaceState.previousSpace, minY: midY + 1 }
+                break;
+            case Direction.LEFT:
+                this.spaceState.space = { ...this.spaceState.previousSpace, maxX: midX }
+                break;
+            case Direction.RIGHT:
+                this.spaceState.space = { ...this.spaceState.previousSpace, minX: midX }
+                break;
+            default:
+                break;
+        }
 
         // 2. Calculate x, y positions again
         // let minY = newSpace.minY;
         // let maxY = newSpace.maxY;
-        // let minX = 1000;
-        // let maxX = 0;
+        // let minX = 1000, maxX = 0;
         // const lineStart = editor.visibleRanges[0].start.line;
         // for (let i = lineStart + newSpace.minY; i <= lineStart + newSpace.maxY; i++) {
         //     minX = Math.min(minX, editor.document.lineAt(i).firstNonWhitespaceCharacterIndex)
@@ -78,127 +121,76 @@ export class CursorMover {
         let newCursorX = currentCursor.character;
         let newCursorY = currentCursor.line;
 
-        newCursorX = Math.min(newCursorX, newSpace.maxX); // move left
-        newCursorX = Math.max(newCursorX, newSpace.minX); // move right
+        newCursorX = Math.min(newCursorX, this.spaceState.space.maxX); // move left
+        newCursorX = Math.max(newCursorX, this.spaceState.space.minX); // move right
         const lineStart = editor.visibleRanges[0].start.line;
-        newCursorY = Math.min(newCursorY, newSpace.maxY + lineStart); // move up
-        newCursorY = Math.max(newCursorY, newSpace.minY + lineStart); // move down
-        
+        newCursorY = Math.min(newCursorY, this.spaceState.space.maxY + lineStart); // move up
+        newCursorY = Math.max(newCursorY, this.spaceState.space.minY + lineStart); // move down
+
         const newCursorPosition = new vscode.Position(newCursorY, newCursorX);
         const newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
         editor.selections = [newSelection];
+        // const cursorPosition = editor.selection.active;
+        // const newRange = new vscode.Range(
+        //     cursorPosition.with(cursorPosition.line, cursorPosition.character),
+        //     // Value can't be negative
+        //     cursorPosition.with(cursorPosition.line, Math.max(0, cursorPosition.character))
+        // );
 
         // 4. Visually remove previous box
-        if (this.spaceTravelState.spaceStyle) {
-            editor.setDecorations(this.spaceTravelState.spaceStyle, []);
+        if (this.spaceState.decoration) {
+            editor.setDecorations(this.spaceState.decoration, []);
         }
 
         // 5. Visually create new box
-        const newStyle = {
-            left: (this.spaceTravelState.charWidth * newSpace.minX).toString() + 'ch',
-            width: (this.spaceTravelState.charWidth * (newSpace.maxX - newSpace.minX)).toString() + 'ch',
-            top: (this.spaceTravelState.charHeight * newSpace.minY).toString() + 'pt',
-            height: (this.spaceTravelState.charHeight * (newSpace.maxY - newSpace.minY + 1)).toString() + 'pt'
+        const boxCss: CssObject = {
+            position: 'absolute',
+            display: 'inline-block',
+            left: (this.spaceState.charWidth * this.spaceState.space.minX).toString() + 'ch',
+            width: (this.spaceState.charWidth * (this.spaceState.space.maxX - this.spaceState.space.minX)).toString() + 'ch',
+            top: (this.spaceState.charHeight * this.spaceState.space.minY).toString() + 'px',
+            height: (this.spaceState.charHeight * (this.spaceState.space.maxY - this.spaceState.space.minY + 1)).toString() + 'px',
+            'z-index': 1,
+            'pointer-events': 'none',
+            'background-color': '#CCCCCC22',
+            border: '1px solid black',
+            'border-radius': '1ch'
         }
-        this.spaceTravelState.spaceStyle = vscode.window.createTextEditorDecorationType(<vscode.DecorationRenderOptions>{
+        this.spaceState.decoration = vscode.window.createTextEditorDecorationType(<vscode.DecorationRenderOptions>{
             before: {
                 contentText: '',
-                textDecoration: `none; position: absolute; display: inline-block; z-index: 1; pointer-events: none; left: ${newStyle.left}; width: ${newStyle.width}; top: ${newStyle.top}; height: ${newStyle.height}; background-color: #CCCCCC22; border: 1px solid black; border-radius: 1ch;`
+                textDecoration: `none; ${StyleCalculator.cssToString(boxCss)}`
             }
         });
         const viewportStart = editor.visibleRanges[0];
-        editor.setDecorations(this.spaceTravelState.spaceStyle, [viewportStart]);
-    }
-
-    // Stop the current space travel
-    private stopSpaceTravel(editor: vscode.TextEditor) {
-        this.spaceTravelState.active = false;
-        if (this.spaceTravelState.spaceStyle) {
-            editor.setDecorations(this.spaceTravelState.spaceStyle, []);
-        }
-        this.spaceTravelState.spaceStyle = null;
-        return;
-    }
-
-    // Start a new space travel
-    private newSpaceTravel(editor: vscode.TextEditor) {
-        const lineStart = editor.visibleRanges[0].start.line;
-        const lineEnd = editor.visibleRanges[0].end.line;
-
-        // Compute minimum and maximum possible x values
-        let minX = 1000;
-        let maxX = 0;
-        for (let i = lineStart; i <= lineEnd; i++) {
-            minX = Math.min(minX, editor.document.lineAt(i).firstNonWhitespaceCharacterIndex);
-            maxX = Math.max(maxX, editor.document.lineAt(i).range.end.character);
-        }
-
-        const newSpace: Box = { minX, maxX, minY: 0, maxY: lineEnd - lineStart };
-        this.spaceTravel(editor, newSpace);
+        editor.setDecorations(this.spaceState.decoration, [viewportStart]);
     }
 
     public activate(context: vscode.ExtensionContext) {
+        // Initializing
         const lineHeight: number = vscode.workspace.getConfiguration('editor').get('lineHeight') ?? 0;
         const fontSize: number = vscode.workspace.getConfiguration('editor').get('fontSize') ?? 14;
-        console.log('CONFIG', lineHeight, fontSize);
+        // Determine line height
+        this.spaceState.charHeight = StyleCalculator.calculateLineHeight(lineHeight, fontSize);
 
+        // i, j, k, l keybindings
         context.subscriptions.push(vscode.commands.registerTextEditorCommand('zim.moveCursorUp', (editor: vscode.TextEditor) => {
-            if (this.spaceTravelState.active) {
-                let midY = Math.floor((this.spaceTravelState.currentSpace.minY + this.spaceTravelState.currentSpace.maxY) / 2);
-                this.spaceTravel(editor, { ...this.spaceTravelState.currentSpace, maxY: midY })
-            } else {
-                CursorMover.moveUp();
-            }
+            this.spaceState.active ? this.spaceTravel(editor, Direction.UP) : this.move(Direction.UP);
         }));
-    
         context.subscriptions.push(vscode.commands.registerTextEditorCommand('zim.moveCursorDown', (editor: vscode.TextEditor) => {
-            if (this.spaceTravelState.active) {
-                let midY = Math.floor((this.spaceTravelState.currentSpace.minY + this.spaceTravelState.currentSpace.maxY) / 2);
-                this.spaceTravel(editor, { ...this.spaceTravelState.currentSpace, minY: midY + 1 })
-            } else {
-                CursorMover.moveDown();
-            }
+            this.spaceState.active ? this.spaceTravel(editor, Direction.DOWN) : this.move(Direction.DOWN);
         }));
-    
         context.subscriptions.push(vscode.commands.registerTextEditorCommand('zim.moveCursorLeft', (editor: vscode.TextEditor) => {
-            if (this.spaceTravelState.active) {
-                let midX = Math.floor((this.spaceTravelState.currentSpace.minX + this.spaceTravelState.currentSpace.maxX) / 2);
-                this.spaceTravel(editor, { ...this.spaceTravelState.currentSpace, maxX: midX })
-            } else {
-                CursorMover.moveLeft();
-            }
+            this.spaceState.active ? this.spaceTravel(editor, Direction.LEFT) : this.move(Direction.LEFT);
         }));
-        
         context.subscriptions.push(vscode.commands.registerTextEditorCommand('zim.moveCursorRight', (editor: vscode.TextEditor) => {
-            if (this.spaceTravelState.active) {
-                let midX = Math.floor((this.spaceTravelState.currentSpace.minX + this.spaceTravelState.currentSpace.maxX) / 2);
-                this.spaceTravel(editor, { ...this.spaceTravelState.currentSpace, minX: midX })
-            } else {
-                CursorMover.moveRight();
-            }
+            this.spaceState.active ? this.spaceTravel(editor, Direction.RIGHT) : this.move(Direction.RIGHT);
         }));
 
+        // Space keybinding
         context.subscriptions.push(vscode.commands.registerTextEditorCommand('zim.space', (editor: vscode.TextEditor) => {
-            if (this.spaceTravelState.active) {
-                // Stop space travel
-                this.stopSpaceTravel(editor);
-            } else {
-                // Start new space travel
-                this.newSpaceTravel(editor);
-            }
-
-            // const cursorPosition = editor.selection.active;
-            // const newRange = new vscode.Range(
-            //     cursorPosition.with(cursorPosition.line, cursorPosition.character),
-            //     // Value can't be negative
-            //     cursorPosition.with(cursorPosition.line, Math.max(0, cursorPosition.character))
-            // );
-
-            // vscode.window.showInformationMessage("tab time start");
-            // const timer = setTimeout(() => {
-            //     vscode.window.showInformationMessage("tab time end");
-            // }, 2000);
-            // const full_range = [new vscode.Range(new vscode.Position(0, 0), new vscode.Position(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER))];
+            // Toggle Space Travel
+            this.spaceState.active ? this.stopSpaceTravel(editor) : this.startSpaceTravel(editor);
         }));
     }
 }
---
+++ src/util/styleCalculator.ts
@@ -0,0 +1,28 @@
+import * as vscode from 'vscode';
+
+export type CssObject = { [key: string]: string | number }
+
+export class StyleCalculator {
+    /**
+     * Calculate line height
+     * @param lineHeight VSCode 'editor.lineHeight' configuration
+     * @param fontSize VSCode 'editor.fontSize' configuration
+     * @returns Line height in pixels
+     */
+    public static calculateLineHeight(lineHeight: number, fontSize: number): number {
+        const fontSizeToPixels = Math.ceil(fontSize * 1.333);
+        if (lineHeight === 0) {
+            return fontSizeToPixels;
+        } else if (lineHeight < 8) {
+            return fontSizeToPixels * lineHeight;
+        } else {
+            return lineHeight;
+        }
+    }
+
+    public static cssToString(cssObject: CssObject) {
+        return Object.keys(cssObject)
+            .map(property => `${property}: ${cssObject[property]};`)
+            .join(' ');
+    }
+}
